d6c5943821bb1c78daa0ab04001b452fba8c3300
Sailesh Sriram <sailesh@sailesh.(none)>
2014-10-14 22:11:00 +0530

    Removed file

--- First.py
+++
@@ -1,8 +0,0 @@
-num_array = list()
-num = raw_input("Enter n value")
-print 'Enter contents of the array : '
-for i in range(int(num)):
-    n = raw_input("")
-    num_array.append(int(n))
-print 'ARRAY: ',num_array
-
