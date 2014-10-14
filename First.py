08262bba7cc0f8744a31584abdb695512d480f37
Sailesh Sriram <sailesh@sailesh.(none)>
2014-10-14 22:08:10 +0530

    TestFile

---
+++ First.py
@@ -0,0 +1,8 @@
+num_array = list()
+num = raw_input("Enter n value")
+print 'Enter contents of the array : '
+for i in range(int(num)):
+    n = raw_input("")
+    num_array.append(int(n))
+print 'ARRAY: ',num_array
+
